4cb64388d2beec7255cff6c3e8b715af0c40ab03
Sean O'Coin <sean@sphereishere.com>
2022-01-24 19:02:21 -0800

    we can make this a little neater

--- knexfile.js
+++ knexfile.js
@@ -1,65 +1,24 @@
 // Update with your config settings.
+const dbConfig = {
+    client: 'postgresql',
+    connection: {
+      database: 'test',
+      user:     'testuser',
+      password: 'testpassword'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    seeds: {
+      directory: './db/seeds'
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'knex_migrations'
+    }
+};
 
 module.exports = {
-
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: 'test',
-      user:     'testuser',
-      password: 'testpassword'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    seeds: {
-      directory: './db/seeds'
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'knex_migrations'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'test',
-      user:     'testuser',
-      password: 'testpassword'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    seeds: {
-      directory: './db/seeds'
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'test',
-      user:     'testuser',
-      password: 'testpassword'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    seeds: {
-      directory: './db/seeds'
-    },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'knex_migrations'
-    }
-  }
-
+  development: dbConfig, staging: dbConfig, production: dbConfig
 };
